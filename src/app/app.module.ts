fbc491ebf3579f3c5ae4c16eddbdcba753cc8a79
zhongxiao.gao <zhongxiao.gao@jingli365.com>
2017-12-12 17:54:03 +0800

    2123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { ChartsComponent } from './charts/charts.component';
 import { ChartsModule } from 'ng2-charts';
 import {FormsModule} from "@angular/forms";
 import { ButtonsModule } from 'ngx-bootstrap';
+import { ProjectManageComponent } from './project-manage/project-manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartsComponent
+    ChartsComponent,
+    ProjectManageComponent
   ],
   imports: [
     BrowserModule,
