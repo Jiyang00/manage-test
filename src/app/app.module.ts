b976588f1d83ffdacb73f9f429f11238a20ca444
jixiang.wang <jixiang.wang@jingli365.com>
2017-12-12 11:17:26 +0800

    提交info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BasicsComponent } from './basics/basics.component';
+import { InfoComponent } from './basics/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
